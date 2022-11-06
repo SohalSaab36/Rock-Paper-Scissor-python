d91df7417e0bf7afd9c1cfaafa1022a8d5cd5416
SohalSaab36 <96253854+SohalSaab36@users.noreply.github.com>
2022-11-06 21:33:49 +0530

    v 1.2

--- rps.py
+++ rps.py
@@ -1,15 +1,17 @@
 import random as rd
 possible = ["rock","paper","scissor"]
 def main():
-  player = input("rock paper scissor = ")
+  player = input("rock paper scissor exit = ")
   cpu = rd.choice(possible)
   print(f"cpu = {cpu}")
   if player == cpu:
     print("draw")
     main()
+  elif player == "exit":
+    exit()
   elif player != cpu:
     if player == possible[0]:
-      if cpu == possible[1]:
+      if cpu === possible[1]:
         print("cpu won")
       elif cpu == possible[2]:
         print("player won")
