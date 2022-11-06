19167a15f49c1380132419780543e5379b8a16e0
SohalSaab36 <96253854+SohalSaab36@users.noreply.github.com>
2022-11-06 15:04:11 +0530

    V 1

---
+++ README.md
@@ -0,0 +1,14 @@
+- 👋 Hi, I’m @SohalSaab36
+- 👀 I’m interested in Programming,console games
+- 🌱 I’m currently learning C++
+- 💞️ I’m looking to collaborate on fellows
+- 📫 How to reach me raja.sohal763@gmail.vom
+
+<!---
+SohalSaab36/SohalSaab36 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
+<h1>ROCK PAPER SCISSOR</h1>
+<pre>it is a popular game that i recreated in python 
+Its console based
+</pre>
---
+++ rps.py
@@ -0,0 +1,38 @@
+import random as rd
+possible = ["rock","paper","scissor"]
+def main():
+  player = input("rock paper scissor = ")
+  cpu = rd.choice(possible)
+  print(f"cpu = {cpu}")
+  if player == cpu:
+    print("draw")
+    main()
+  elif player != cpu:
+    if player == possible[0]:
+      if cpu == possible[1]:
+        print("cpu won")
+      elif cpu == possible[2]:
+        print("player won")
+      else:
+        print("Something went wrong")
+      main()
+    elif player == possible[1]:
+      if cpu ==  possible[0]:
+        print("player won")
+      elif cpu == possible[2]:
+        print("cpu won")
+      else:
+        print("Something went wrong")
+      main()
+    elif player == possible[2]:
+      if cpu == possible[0]:
+        print("cpu won")
+      elif cpu == possible[1]:
+        print("player won")
+      else:
+        print("something went wrong")
+      main()
+    else:
+      print("err")
+      main()
+main()
