0762fa5c7a4c3aa2564e90af1ab3a0e552c67d31
SohalSaab36 <96253854+SohalSaab36@users.noreply.github.com>
2022-11-07 23:03:02 +0530

    v2.0-score upadate
    added point and fixed bugs

--- rps.py
+++ rps.py
@@ -1,55 +1,52 @@
 import random as rd
 possible = ["rock","paper","scissor"]
-def main():
-  cscore = 0 
+while True:
+  cscore = 0
   pscore = 0
-  player = input("rock paper scissor ; type exit to end = ")
+  player = input("rock paper scissor ; type exit to end = ").lower()
   cpu = rd.choice(possible)
   print(f"cpu = {cpu}")
-  print(f"{Cscore}cpu {Pscore}player")
+  print(f"{cscore}cpu {cscore}player")
   if player == cpu:
     print("Tie! ; 0 points added")
-    main()
   elif player == "exit":
-    exit()
+    break
   elif player != cpu:
     if player == possible[0]:
       if cpu == possible[1]:
         print("cpu won")
         cscore = cscore + 1
-        print(f"{Cscore}cpu {Pscore}player")
+        print(f"{cscore}cpu {cscore}player")
       elif cpu == possible[2]:
         print("player won")
         pscore = pscore +1
-        print(f"{Cscore}cpu {Pscore}player")
+        print(f"{cscore}cpu {pscore}player")
       else:
         print("Something went wrong")
-      main()
+
     elif player == possible[1]:
       if cpu ==  possible[0]:
         print("player won")
         pscore = pscore + 1
-        print(f"{Cscore}cpu {Pscore}player")
+        print(f"{cscore}cpu {cscore}player")
       elif cpu == possible[2]:
         print("cpu won")
         cscore = cscore +1
-        print(f"{Cscore}cpu {Pscore}player")
+        print(f"{cscore}cpu {pscore}player")
       else:
         print("Something went wrong")
-      main()
+
     elif player == possible[2]:
       if cpu == possible[0]:
         print("cpu won")
         cscore = cscore +1
-        print(f"{Cscore}cpu {Pscore}player")
+        print(f"{cscore}cpu {pscore}player")
       elif cpu == possible[1]:
         print("player won")
         pscore = pscore +1
-        print(f"{Cscore}cpu {Pscore}player")
+        print(f"{cscore}cpu {pscore}player")
       else:
         print("something went wrong")
-      main()
+
     else:
       print("err")
-      main()
-main()
