2e3b18d6dec64d0bdcee8eb528bbc7460d189fe8
SohalSaab36 <96253854+SohalSaab36@users.noreply.github.com>
2022-11-07 15:55:55 +0530

    v 1.3

--- rps.py
+++ rps.py
@@ -1,11 +1,14 @@
 import random as rd
+Pscore = 0 #player
+Cscore = 0 #cpu
 possible = ["rock","paper","scissor"]
 def main():
   player = input("rock paper scissor exit = ")
   cpu = rd.choice(possible)
   print(f"cpu = {cpu}")
+  print(f"{Cscore}cpu {Pscore}player")
   if player == cpu:
-    print("draw")
+    print("draw 0 points added")
     main()
   elif player == "exit":
     exit()
@@ -13,24 +16,36 @@ def main():
     if player == possible[0]:
       if cpu == possible[1]:
         print("cpu won")
+        Cscore = Cscore + 1
+        print(f"{Cscore}cpu {Pscore}player")
       elif cpu == possible[2]:
         print("player won")
+        Pscore = Pscore +1
+        print(f"{Cscore}cpu {Pscore}player")
       else:
         print("Something went wrong")
       main()
     elif player == possible[1]:
       if cpu ==  possible[0]:
         print("player won")
+        Pscore = Pscore + 1
+        print(f"{Cscore}cpu {Pscore}player")
       elif cpu == possible[2]:
         print("cpu won")
+        Cscore = Cscore +1
+        print(f"{Cscore}cpu {Pscore}player")
       else:
         print("Something went wrong")
       main()
     elif player == possible[2]:
       if cpu == possible[0]:
         print("cpu won")
+        Cscore = Cscore +1
+        print(f"{Cscore}cpu {Pscore}player")
       elif cpu == possible[1]:
         print("player won")
+        Pscore = Pscore +1
+        print(f"{Cscore}cpu {Pscore}player")
       else:
         print("something went wrong")
       main()
