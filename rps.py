cdd002e0b976662a83ad6e52b9aafe3d3806f7c6
SohalSaab36 <96253854+SohalSaab36@users.noreply.github.com>
2023-01-01 16:17:23 +0530

    Full rebuild

--- rps.py
+++ rps.py
@@ -1,86 +1,71 @@
-import random as rd
-p =    ["rock","paper","scissor"]
-TS = 0
-cs =    0
-ps =    0
-RUN =    True
-def conf():
-  while True:
-        pl = input("continue (y/n): ").lower()
-        if pl == "y":
-          break
-        else:
-           if pl == "n":
-                if cs ==    ps:
-                    if TS >    0:
-                        print("tied")
-                    else:
-                        print("no scores :)")
-                elif cs >    ps:
-                    print("you lose")
-                elif cs <    ps:
-                    print("player won")
-                else:
-                    print("debug: conf()")
-                exit()
-           else:
-                if pl == "y":
-                    break
-                else:
-                    conf()
-while RUN:
-            TS =    cs + ps
-            player =    input(f"{p}: ").lower()
-            cpu =    rd.choice(p)
-            print(f"cpu chose {cpu}")
-            print(f"cpu score {cs}   ; player score {ps}")
-            
-            if player == cpu:
-                print("as usual tied")
-                conf()
-            elif player !=    cpu and player in p:
-                if player ==    p[0]:
-                    if cpu ==    p[1]:
-                        print("you lose")
-                        cs =    cs + 1
-                        print(f"cpu score {cs}   ; player score {ps}")
-                        conf()
-                    elif cpu==p[2]:
-                        print("player won")
-                        ps =    ps + 1
-                        print(f"cpu score {cs}   ; player score {ps}")
-                        conf()
-                    else:
-                       print("RESTART")
-                       conf()
-                elif player ==    p[1]:
-                     if cpu ==    p[0]:
-                         print("you won")
-                         ps =    ps+1
-                         print(f"cpu score {cs}   ; player score {ps}")
-                         conf()
-                     elif cpu ==    p[2]:
-                         print("you lose")
-                         cs =    cs+1
-                         print(f"cpu score {cs}   ; player score {ps}")
-                         conf()
-                     else:
-                         print("RESTART")
-                         conf()
-                elif player ==    p[2]:
-                    if cpu ==    p[0]:
-                        print("you lose")
-                        cs =    cs+1
-                        print(f"cpu score {cs}   ; player score {ps}")
-                        conf()
-                    elif cpu ==    p[1]:
-                        print("you won")
-                        ps =    ps+1
-                        print(f"cpu score {cs}   ; player score {ps}")
-                        conf()
-                    else:
-                        print("RESTART")
-                        conf()                       
-            else:
-                   print("wrong input restart")
-                   conf()
+'''
+##    ###        #       ##
+#  #  #----       # #     #  #
+##    #         # # #   #  #
+#  #  ###  #       #  ##
+Hello!
+Made by SohalSaab36 
+COPYRIGHT
+Original Source : GitHub
+Happy Weekend
+current version v 5.1
+##    ##   ###
+#  # # #  #----
+#   #   #  #
+#        #  ###
+'''
+import random
+
+def play_game():
+  # list of possible moves
+  moves = ['rock', 'paper', 'scissors']
+
+  # get player's move
+  player_move = input(f"{moves}:").lower()
+  if player_move not in moves:
+    print("Invalid move. Please enter rock, paper, or scissors.")
+    return
+
+  # get computer's move
+  computer_move = random.choice(moves)
+  print(f"player choice {player_move} and computer choice {computer_move}")  
+    
+  # determine the winner
+  if player_move == computer_move:
+    print("It's a tie!")
+  elif (player_move == 'rock' and computer_move == 'scissors') or \
+       (player_move == 'paper' and computer_move == 'rock') or \
+       (player_move == 'scissors' and computer_move == 'paper'):
+    print("You win!")
+    return 1 # return 1 point for player
+  else:
+    print("Computer wins!")
+    return -1 # return -1 point for computer
+
+# initialize scores
+player_score = 0
+computer_score = 0
+
+# play the game multiple times
+while True:
+  result = play_game()
+  if result == 1:
+    player_score += 1
+  elif result == -1:
+    computer_score += 1
+  else:
+    pass
+
+  # print scores
+  print(f"Player: {player_score} | Computer: {computer_score}")
+
+  # ask if player wants to continue
+  continue_playing = input("Do you want to play again? (y/n) ").lower()
+  if continue_playing == 'y':
+    pass
+  elif continue_playing == "n":
+      break
+  else:
+      pass
+
+print("Thanks for playing!")
